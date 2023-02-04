cc89ae2c90235b30533f4090d61a6b6e887b06af
Vladyslav <vladyslav.s@code-care.pro>
2023-02-04 16:35:52 +0200

    add crud user

--- api/app.js
+++ api/app.js
@@ -1,6 +1,23 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 const port = process.env.PORT || 4000;
+const db = require("./crud");
+
+app.use(bodyParser.json());
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.get("/users", db.getUsers);
+app.get("/users/:id", db.getUserById);
+app.post("/users", db.createUser);
+app.put("/users/:id", db.updateUser);
+app.delete("/users/:id", db.deleteUser);
+
 app.get("/", (req, res) => {
   res.send("Home Route");
 });
